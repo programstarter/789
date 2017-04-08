4a2153a19172d1853382a2a36caf2ca2726029dd
Vincent <vincent@VincentdeMacBook-Pro-2.local>
2017-04-08 17:30:43 +0800

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$html = <<< HEREDOC
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Practice</title>
+</head>
+<body>
+	<h1>PHP Practice</h1>
+	<a href="HomeworkFirst.php"><h2>After Class: Homework 1</h2></a>
+	<a href="HomeworkSecondQ.php"><h2>After Class: Homework 2</h2></a>
+</body>
+</html>
+HEREDOC;
+
+echo $html;
+
+?>
